02f8232d2bd7853ea3e049290cd5ee2e28cf0a16
Joe Klinck <jklinck76@gmail.com>
2025-05-29 20:03:53 -0700

    created struct, map and csv reader

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <string>
 using namespace std;
+
+struct Phone{
+  string oem;
+  string model;
+  string launchAnnounced;
+  string launchStatus;
+  string bodyDimensions;
+  string bodyWeight;
+  string bodySim;
+  string displayType;
+  string displaySize;
+  string displayResoltuion;
+  string featuresSensors;
+  string platformOS;  
+};
 
 int main(){
   
-  cout << "Test" << endl;
+  unorder_map<int, Phone> map;
+  ifstream file("cells.csv");
+  string line;
+
+  getline(file, line);
+
+  
 
   return 0;
 }
